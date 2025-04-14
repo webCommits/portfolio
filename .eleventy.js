d65474adbce1d87a902f0aeff9e2c637b0e49e44
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-04-14 16:29:43 +0200

    Added first idea from template

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("src/static/script.js")
     eleventyConfig.addPassthroughCopy("src/static/images")
     eleventyConfig.addPassthroughCopy("src/static/svg")
+    eleventyConfig.addPassthroughCopy("src/assets")
     eleventyConfig.addPassthroughCopy("src/sitemap.xml")
     eleventyConfig.addPassthroughCopy("src/robots.txt")
     return {
